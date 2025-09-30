f430076c2cd5d18f6db12a87cdc9301f9cc2b85f
Praveena G <praveenaganesan36054@gmaile.com>
2025-09-30 13:45:33 +0530

    feat(user): add new user-related routes to controller

---
+++ app/dtos/UserDtos.scala
@@ -0,0 +1,38 @@
+package app.dtos
+
+import play.api.libs.json._
+import app.models.User
+
+case class CreateUserRequest(
+    name :String,
+    email :String
+)
+
+case class UserResponse (
+    id: Long,
+    name :String,
+    email :String
+)
+
+object CreateUserRequest{
+    implicit val format :OFormat[CreateUserRequest] =Json.format[CreateUserRequest]
+}
+
+object UserResponse{
+    implicit val format :OFormat[UserResponse]= Json.format[UserResponse]
+    def fromUser(user: User): UserResponse = {
+    UserResponse(
+      id = user.id.getOrElse(throw new IllegalStateException("User must have an ID")),
+      name = user.name,
+      email = user.email
+    )
+  }
+}
+
+case class ErrorResponse (
+    error :String,
+    details :Option[Map[String,String]] = None
+)
+object ErrorResponse{
+    implicit val format :OFormat[ErrorResponse] =Json.format[ErrorResponse]
+}
